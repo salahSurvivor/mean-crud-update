3e43c4ecd22b6f411f50195d7f0fa64b49f28f68
salahSurvivor <salaheddinenaji2000@gmail.com>
2023-05-17 18:05:45 +0100

    bcrypt password

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -19,7 +19,7 @@ router.post('/register', async(req, res) =>{
         const data = {
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: await bcrypt.hash(req.body.password, 10),
             isAdmin: req.body.isAdmin
         }
         const user = await Users.create(data);
@@ -36,21 +36,18 @@ router.post('/login', async(req, res) => {
     const password = req.body.password;
     const users = await Users.find({});
     
-    var userFound = false;
-    
-    users.forEach(vl => {
-        if(username === vl.name && password === vl.password) {
-            // Generate a JWT token
-            const token = jwt.sign({id: vl._id, name: username, email: vl.email, isAdmin: vl.isAdmin }, 'secret', { expiresIn: '1h' });
-            res.status(200).json({ token });
-            userFound = true;
+    for(vl of users){
+        if(username === vl.name){
+            const passwordMatch = await bcrypt.compare(password, vl.password);
+            if(passwordMatch){
+                // Generate a JWT token
+                const token = jwt.sign({id: vl._id, name: username, email: vl.email, isAdmin: vl.isAdmin }, 'secret', { expiresIn: '1h' });
+                return res.status(200).json({ token });
+            }
         }
-    });
+    };
 
-    if(!userFound){
-        res.status(500).json({ message: 'Invalid credentials' });
-    }
-
+    return res.status(500).json({ message: 'Invalid credentials' });
 });
 
 router.put('/users/:id', async(req, res) => {
